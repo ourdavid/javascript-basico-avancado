395d76b823a858bb7c3335ff628f354645f7b830
David William <Contatodavidwilliams@gmail.com>
2024-12-07 20:26:54 -0300

    boolean test

---
+++ teoria/1-introducao/3-boolean/script.js
@@ -0,0 +1,12 @@
+//o boolean sempre retorar true ou false
+
+const num1 = 1
+const string = 'test'
+
+console.log(typeof(num1) == 'number')
+console.log(5>3)
+console.log(5==='5')
+console.log(233 =='233')
+
+console.log(typeof true)
+console.log(typeof false)
